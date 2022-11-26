46b07e2e94dc35de1debf916f43cff4ac6d28571
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-27 01:35:06 +0300

    Modify greetings controller

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,5 @@
 class GreetingsController < ApplicationController
   def index
-    @message = Greeting.order('random()').first
-    render json: @message
+    render json: Greeting.order('RANDOM()').first
   end
 end
